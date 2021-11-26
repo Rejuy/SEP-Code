99398d8acac10a100c18e73ea64a8bf3b690e838
Viktor <zbwkeepgoing@126.com>
2021-11-26 21:21:36 +0800

    课程列表前端设计完成

--- front_end/pages/course/course.js
+++ front_end/pages/course/course.js
@@ -5,7 +5,32 @@ Page({
      * 页面的初始数据
      */
     data: {
-        image_url: "https://learn.tsinghua.edu.cn/b/wlxt/kc/v_kcxx_jskcxx/teacher/showImageById?wlkcid=2021-2022-1142764790&_csrf=d39592c7-bbb0-416a-affb-a39b1ab00ba4"
+        department_type: [
+            { text: '开课院系', value: 0 },
+            { text: '体育部', value: 1 },
+            { text: '语言中心', value: 2 },
+            { text: '软件学院', value: 3 },
+        ],
+        course_type: [
+            { text: '课程类型', value: 0 },
+            { text: '体育课', value: 1 },
+            { text: '外文课', value: 2 },
+            { text: '文化素质课', value: 3 },
+            { text: '文化素质核心课', value: 4 },
+        ],
+        rank_type: [
+            { text: '排序方式', value: 0 },
+            { text: '评分排序', value: 1 },
+            { text: '热度排序', value: 2 },
+        ],
+        department_value: 0,
+        course_value: 0,
+        rank_value: 0,
+        image_url: "https://learn.tsinghua.edu.cn/b/wlxt/kc/v_kcxx_jskcxx/teacher/showImageById?wlkcid=2021-2022-1142764790&_csrf=d39592c7-bbb0-416a-affb-a39b1ab00ba4",
+
+        current_page: 0,
+        total_pages: 0,
+        courses_list: []
     },
 
     /**
@@ -54,7 +79,12 @@ Page({
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        if(this.data.current_page >= this.data.total_pages) {
+            console.log("已无下一页数据");
+        }else {
+            this.data.current_page ++;
+            // 更新 coursers_list
+        }
     },
 
     /**
