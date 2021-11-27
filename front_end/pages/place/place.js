a955499fe3d7643414d43ef046bea77f78c77f24
Viktor <zbwkeepgoing@126.com>
2021-11-27 20:12:47 +0800

    搭建出行推荐前端页面

--- front_end/pages/place/place.js
+++ front_end/pages/place/place.js
@@ -5,7 +5,28 @@ Page({
      * 页面的初始数据
      */
     data: {
+        range_type: [
+            { text: '任意地点', value: 0 },
+            { text: '校内地点', value: 1 },
+            { text: '校外地点', value: 2 },
+        ],
+        place_type: [
+            { text: '任意场所', value: 0 },
+            { text: '自习场所', value: 1 },
+            { text: '锻炼场所', value: 2 },
+            { text: '会议场所', value: 3 },
+            { text: '娱乐场所', value: 4 },
+        ],
+        rank_type: [
+            { text: '评分排序', value: 0 },
+            { text: '热度排序', value: 1 },
+            { text: '时间排序', value: 2 },
+        ],
+        range_value: 0,
+        place_value: 0,
+        rank_value: 0,
 
+        image_url: "https://mmbiz.qpic.cn/mmbiz_jpg/HhoEMZZMsiaQgcfIVLkACUh2wiaMRyVkiaaxScRDXzvmA4erdq8HzhF34JzQzH7PsjdZRtgcn51XdE93IIiaCZNqUw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1"
     },
 
     /**
