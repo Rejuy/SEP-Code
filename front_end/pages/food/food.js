9a44833a59c57dcc483ce4031bb7a45e24f63395
Viktor <zbwkeepgoing@126.com>
2021-11-27 12:15:33 +0800

    搭建餐饮推荐页面

--- front_end/pages/food/food.js
+++ front_end/pages/food/food.js
@@ -5,7 +5,29 @@ Page({
      * 页面的初始数据
      */
     data: {
+        range_type: [
+            { text: '全部餐饮', value: 0 },
+            { text: '校内餐饮', value: 1 },
+            { text: '校外餐饮', value: 2 },
+        ],
+        food_type: [
+            { text: '任意类型', value: 0 },
+            { text: '汉堡披萨', value: 1 },
+            { text: '龙虾烧烤', value: 2 },
+            { text: '香锅火锅', value: 3 },
+            { text: '米线拉面', value: 4 },
+        ],
+        rank_type: [
+            { text: '评分排序', value: 0 },
+            { text: '热度排序', value: 1 },
+            { text: '时间排序', value: 2 },
+        ],
+        range_value: 0,
+        food_value: 0,
+        rank_value: 0,
+        uncertain_range: true,
 
+        image_url: "https://obohe.com/i/2021/11/27/j1j3qo.jpg"
     },
 
     /**
@@ -26,7 +48,7 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        
     },
 
     /**
