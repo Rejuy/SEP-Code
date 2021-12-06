608929950a4ffb27802928ba1bf5bd1249de3c2a
Viktor <zbwkeepgoing@126.com>
2021-12-06 23:23:53 +0800

    item样式基本渲染完成

--- front_end/pages/course_item/course_item.js
+++ front_end/pages/course_item/course_item.js
@@ -15,10 +15,17 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        console.log('ready');
         this.setData({
             loading: false,
         })
+    },
+
+    giveLikes: function() {
+        console.log("likes");
+    },
+
+    viewDetails: function (options) {
+        console.log("hit");
     },
 
     /**
