83f3dd58e57e2d93f50bc80ff29b666728667c76
Rejuy <3106528078@qq.com>
2021-12-19 09:33:42 +0800

    增加局部搜索

--- back_end/services/food_service.py
+++ back_end/services/food_service.py
@@ -38,6 +38,10 @@ def getFoodsList(raw_info):
         new_info['sort_criteria'] = 'time'
     else:
         new_info['sort_criteria'] = 'star'
+
+    if "like" in raw_info.keys():
+        new_info['like'] = raw_info['like']
+
     # 获取列表
     raw_list, food_count, result = db.getItemList("food_list", new_info)
     if result:
