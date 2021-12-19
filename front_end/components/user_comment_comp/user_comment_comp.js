04055c6f42778235a0424d1f2c57b381e5400254
Onii Chan <46487694+sogood99@users.noreply.github.com>
2021-12-19 10:20:23 +0800

    修改样式

--- front_end/components/user_comment_comp/user_comment_comp.js
+++ front_end/components/user_comment_comp/user_comment_comp.js
@@ -48,7 +48,6 @@ Component({
   lifetimes: {
     ready() {
       if (this.properties.is_self) {
-        console.log(this.properties.imageurls);
         this.setData({
           tag: "自己",
         })
