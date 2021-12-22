8c0c23cf16738a5eb56f4d170f960f126e548233
Onii Chan <46487694+sogood99@users.noreply.github.com>
2021-12-22 22:18:39 +0800

    更新register提示

--- front_end/pages/register/register.js
+++ front_end/pages/register/register.js
@@ -43,10 +43,24 @@ Page({
         },
         method: "POST",
         success: function (res) {
-          if (res.data === 0) {
+          if (res.data.state === 0) {
             Notify({ type: "success", message: "邮件已发送，请查看邮箱" });
-          } else {
-            Notify({ type: "danger", message: "信息异常，请检查注册信息" });
+          } else if (res.data === 1) {
+            Notify({ type: "danger", message: "邮箱不合法，请检查注册信息" });
+          }else if (res.data === 2) {
+            Notify({ type: "danger", message: "用户名不合法，请检查注册信息" });
+          }else if (res.data === 3) {
+            Notify({ type: "danger", message: "密码长度不合法，请检查注册信息" });
+          }else if (res.data === 4) {
+            Notify({ type: "danger", message: "密码符号类型不足，请检查注册信息" });
+          }else if (res.data === 5) {
+            Notify({ type: "danger", message: "密码包含非法字符，请检查注册信息" });
+          }else if (res.data === 6) {
+            Notify({ type: "danger", message: "邮箱已存在，请检查注册信息" });
+          }else if (res.data === 7) {
+            Notify({ type: "danger", message: "用户名已存在，请检查注册信息" });
+          }else{
+            Notify({ type: "danger", message: "连接失败，请稍后重试" });
           }
         },
         fail: function (res) {
