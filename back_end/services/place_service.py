bf7124cdb4579a21b480a6f95b3dcf39717aa86f
Rejuy <3106528078@qq.com>
2021-12-23 02:18:37 +0800

    调整我的创作

--- back_end/services/place_service.py
+++ back_end/services/place_service.py
@@ -1,5 +1,6 @@
 from headers import *
 from services.mysql_service import db
+from services.code_service import coder
 
 
 def getPlacesList(raw_info):
@@ -26,6 +27,8 @@ def getPlacesList(raw_info):
         new_info['filter'].append({"key": "type", "value": raw_info['place_type']})
     if raw_info['place_scope'] != 0:
         new_info['filter'].append({"key": "scope", "value": raw_info['place_scope']})
+    if 'mask' in raw_info.keys():
+        new_info['filter'].append({"key": "user_id", "value": coder.decode(raw_info['mask'])})
     # 改变sort_order至函数需要
     new_info['sort_order'] = 'desc'
     # if raw_info['sort_order'] == ASCENDING:
