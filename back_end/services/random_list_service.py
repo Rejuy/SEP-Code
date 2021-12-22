2701aae97906499e07f1121448ef0eb256dd8e61
Rejuy <3106528078@qq.com>
2021-12-22 22:31:40 +0800

    增加随机漫步

--- back_end/services/random_list_service.py
+++ back_end/services/random_list_service.py
@@ -9,5 +9,27 @@ def randomList():
     course_index = random.randint(0, course_count - 1)
     food_index = random.randint(0, food_count - 1)
     place_index = random.randint(0, place_count - 1)
-    # course_id = db.getData("course_list", [], [], ["id"], get_all=)
-    pass
+    course_id, flag1 = db.randomItemId("course_list", course_index)
+    if not flag1:
+        return []
+    food_id, flag2 = db.randomItemId("food_list", food_index)
+    if not flag2:
+        return []
+    place_id, flag3 = db.randomItemId("place_list", place_index)
+    if not flag3:
+        return []
+    return [
+        {
+            "type": 1,
+            "id": course_id
+        },
+        {
+            "type": 2,
+            "id": food_id
+        },
+        {
+            "type": 3,
+            "id": place_id
+        }
+    ]
+
