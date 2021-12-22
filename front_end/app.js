c5c225532e3b6bbb8aadd38de7534dfed767223a
Onii Chan <46487694+sogood99@users.noreply.github.com>
2021-12-23 03:26:35 +0800

    删除初始用户

--- front_end/app.js
+++ front_end/app.js
@@ -2,7 +2,7 @@
 App({
   global_data: {
     global_domain: 'https://thurec.xyz',
-    global_user_token: "c+O9LCj7q77CfNc7yH3ZEg==", // user login token
+    global_user_token: "", // user login token
   },
   logout: function(){
     this.global_user_token = ""
