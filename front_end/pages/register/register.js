3b405150111fc1b2c0b26cb995ab6aa3a53e2332
sogood99 <dingjh19@mails.tsinghua.edu.cn>
2021-12-24 16:50:05 +0800

    丰富注册提示信息

--- front_end/pages/register/register.js
+++ front_end/pages/register/register.js
@@ -30,7 +30,7 @@ Page({
       // attempt register
       const app = getApp();
       const domain = app.global_data.global_domain;
-      let register_domain = domain + '/api/v1.0/register_user_info';
+      let register_domain = domain + "/api/v1.0/register_user_info";
       wx.request({
         url: register_domain,
         data: {
@@ -44,22 +44,38 @@ Page({
         method: "POST",
         success: function (res) {
           if (res.data.state === 0) {
-            Notify({ type: "success", message: "邮件已发送，请查看邮箱" });
+            Notify({
+              type: "success",
+              message: "邮件已发送，请查看邮箱(可能在垃圾邮件中)",
+            });
           } else if (res.data.state === 1) {
             Notify({ type: "danger", message: "邮箱不合法，请检查注册信息" });
-          }else if (res.data.state === 2) {
-            Notify({ type: "danger", message: "用户名不合法，请检查注册信息" });
-          }else if (res.data.state === 3) {
-            Notify({ type: "danger", message: "密码长度不合法，请检查注册信息" });
-          }else if (res.data.state === 4) {
-            Notify({ type: "danger", message: "密码符号类型不足，请检查注册信息" });
-          }else if (res.data.state === 5) {
-            Notify({ type: "danger", message: "密码包含非法字符，请检查注册信息" });
-          }else if (res.data.state === 6) {
+          } else if (res.data.state === 2) {
+            Notify({
+              type: "danger",
+              message: "用户名不合法，长度要求 6 到 10",
+            });
+          } else if (res.data.state === 3) {
+            Notify({
+              type: "danger",
+              message: "密码长度不合法，长度要求 6 到 18",
+            });
+          } else if (res.data.state === 4) {
+            Notify({
+              type: "danger",
+              message:
+                "密码符号类型不足，需包含大小写字母、数字、符号，且必须包含前三者",
+            });
+          } else if (res.data.state === 5) {
+            Notify({
+              type: "danger",
+              message: "密码包含非法字符，只能包含大小写字母、数字、符号",
+            });
+          } else if (res.data.state === 6) {
             Notify({ type: "danger", message: "邮箱已存在，请检查注册信息" });
-          }else if (res.data.state === 7) {
+          } else if (res.data.state === 7) {
             Notify({ type: "danger", message: "用户名已存在，请检查注册信息" });
-          }else{
+          } else {
             Notify({ type: "danger", message: "连接失败，请稍后重试" });
           }
         },
